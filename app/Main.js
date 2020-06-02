1623e672c54eca5a1f810e7033126d8a0a99caa7
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-02 11:56:12 +0200

    Creating multiple components and adding react-router-dom

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function ExampleComponent() {
+// My Components
+import Header from './components/Header';
+import HomeGuest from './components/HomeGuest';
+import Footer from './components/Footer';
+import About from './components/About';
+import Terms from './components/Terms';
+
+function Main() {
 	return (
-		<div>
-			<h1>This is our app!!!</h1>
-			<p>The sky is blue and the grass is green.</p>
-		</div>
+		<BrowserRouter>
+			<Header />
+			<Switch>
+				<Route path="/" exact>
+					<HomeGuest />
+				</Route>
+				<Route path="/about-us" exact>
+					<About />
+				</Route>
+				<Route path="/terms" exact>
+					<Terms />
+				</Route>
+			</Switch>
+			<Footer />
+		</BrowserRouter>
 	);
 }
 
-ReactDOM.render(<ExampleComponent />, document.querySelector('#app'));
+ReactDOM.render(<Main />, document.querySelector('#app'));
 
 if (module.hot) {
 	module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
 		port: 3000,
 		contentBase: path.join(__dirname, 'app'),
 		hot: true,
+		historyApiFallback: { index: 'index.html' },
 	},
 	module: {
 		rules: [
