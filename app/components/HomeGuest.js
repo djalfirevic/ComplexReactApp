7ef1b0770ecdb2d2262f0b1b621164ec68cfe7bf
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-05 08:05:45 +0200

    Adding login and register functionality

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Page from './Page';
+import Axios from 'axios';
 
 function HomeGuest() {
+	const [username, setUsername] = useState();
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			await Axios.post('http://localhost:8080/register', {
+				username,
+				email,
+				password,
+			});
+			console.log('User was successfully created.');
+		} catch (error) {
+			console.error('There was an error.');
+		}
+	}
+
 	return (
 		<Page title="Welcome" wide={true}>
 			<div className="row align-items-center">
@@ -15,12 +35,13 @@ function HomeGuest() {
 					</p>
 				</div>
 				<div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-					<form>
+					<form onSubmit={handleSubmit}>
 						<div className="form-group">
 							<label htmlFor="username-register" className="text-muted mb-1">
 								<small>Username</small>
 							</label>
 							<input
+								onChange={(e) => setUsername(e.target.value)}
 								id="username-register"
 								name="username"
 								className="form-control"
@@ -34,6 +55,7 @@ function HomeGuest() {
 								<small>Email</small>
 							</label>
 							<input
+								onChange={(e) => setEmail(e.target.value)}
 								id="email-register"
 								name="email"
 								className="form-control"
@@ -47,6 +69,7 @@ function HomeGuest() {
 								<small>Password</small>
 							</label>
 							<input
+								onChange={(e) => setPassword(e.target.value)}
 								id="password-register"
 								name="password"
 								className="form-control"
