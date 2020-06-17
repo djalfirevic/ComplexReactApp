7f59c4457708ba1ba3c72f91d60d1a30629943e5
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-17 18:44:28 +0200

    Search component completed

--- app/components/Search.js
+++ app/components/Search.js
@@ -2,9 +2,11 @@ import React, { useEffect, useContext } from 'react';
 import DispatchContext from '../DispatchContext';
 import { useImmer } from 'use-immer';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Search() {
 	const appDispatch = useContext(DispatchContext);
+
 	const [state, setState] = useImmer({
 		searchTerm: '',
 		results: [],
@@ -14,28 +16,50 @@ function Search() {
 
 	useEffect(() => {
 		document.addEventListener('keyup', searchKeyPressHandler);
-
-		// This will be called after this component unmounts (cleanup)
-		return () => {
-			document.removeEventListener('keyup', searchKeyPressHandler);
-		};
+		return () => document.removeEventListener('keyup', searchKeyPressHandler);
 	}, []);
 
 	useEffect(() => {
-		const delay = setTimeout(() => {
+		if (state.searchTerm.trim()) {
 			setState((draft) => {
-				draft.requestCount++;
+				draft.show = 'loading';
 			});
-		}, 700);
+			const delay = setTimeout(() => {
+				setState((draft) => {
+					draft.requestCount++;
+				});
+			}, 750);
 
-		return () => clearTimeout(delay);
-	}, state.searchTerm);
+			return () => clearTimeout(delay);
+		} else {
+			setState((draft) => {
+				draft.show = 'neither';
+			});
+		}
+	}, [state.searchTerm]);
 
 	useEffect(() => {
 		if (state.requestCount) {
-			// Send axios request here
+			const ourRequest = Axios.CancelToken.source();
+			async function fetchResults() {
+				try {
+					const response = await Axios.post(
+						'/search',
+						{ searchTerm: state.searchTerm },
+						{ cancelToken: ourRequest.token }
+					);
+					setState((draft) => {
+						draft.results = response.data;
+						draft.show = 'results';
+					});
+				} catch (e) {
+					console.log('There was a problem or the request was cancelled.');
+				}
+			}
+			fetchResults();
+			return () => ourRequest.cancel();
 		}
-	}, state.requestCount);
+	}, [state.requestCount]);
 
 	function searchKeyPressHandler(e) {
 		if (e.keyCode == 27) {
@@ -45,7 +69,6 @@ function Search() {
 
 	function handleInput(e) {
 		const value = e.target.value;
-
 		setState((draft) => {
 			draft.searchTerm = value;
 		});
@@ -77,38 +100,49 @@ function Search() {
 
 			<div className="search-overlay-bottom">
 				<div className="container container--narrow py-3">
-					<div className="live-search-results live-search-results--visible">
-						<div className="list-group shadow-sm">
-							<div className="list-group-item active">
-								<strong>Search Results</strong> (3 items found)
+					<div
+						className={
+							'circle-loader ' +
+							(state.show == 'loading' ? 'circle-loader--visible' : '')
+						}></div>
+					<div
+						className={
+							'live-search-results ' +
+							(state.show == 'results' ? 'live-search-results--visible' : '')
+						}>
+						{Boolean(state.results.length) && (
+							<div className="list-group shadow-sm">
+								<div className="list-group-item active">
+									<strong>Search Results</strong> ({state.results.length}{' '}
+									{state.results.length > 1 ? 'items' : 'item'} found)
+								</div>
+								{state.results.map((post) => {
+									const date = new Date(post.createdDate);
+									const dateFormatted = `${
+										date.getMonth() + 1
+									}/${date.getDate()}/${date.getFullYear()}`;
+
+									return (
+										<Link
+											onClick={() => appDispatch({ type: 'closeSearch' })}
+											key={post._id}
+											to={`/post/${post._id}`}
+											className="list-group-item list-group-item-action">
+											<img className="avatar-tiny" src={post.author.avatar} />{' '}
+											<strong>{post.title}</strong>{' '}
+											<span className="text-muted small">
+												by {post.author.username} on {dateFormatted}{' '}
+											</span>
+										</Link>
+									);
+								})}
 							</div>
-							<a href="#" className="list-group-item list-group-item-action">
-								<img
-									className="avatar-tiny"
-									src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-								/>{' '}
-								<strong>Example Post #1</strong>
-								<span className="text-muted small">by brad on 2/10/2020 </span>
-							</a>
-							<a href="#" className="list-group-item list-group-item-action">
-								<img
-									className="avatar-tiny"
-									src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
-								/>{' '}
-								<strong>Example Post #2</strong>
-								<span className="text-muted small">
-									by barksalot on 2/10/2020{' '}
-								</span>
-							</a>
-							<a href="#" className="list-group-item list-group-item-action">
-								<img
-									className="avatar-tiny"
-									src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-								/>{' '}
-								<strong>Example Post #3</strong>
-								<span className="text-muted small">by brad on 2/10/2020 </span>
-							</a>
-						</div>
+						)}
+						{!Boolean(state.results.length) && (
+							<p className="alert alert-danger text-center shadow-sm">
+								Sorry, we could not find any results for that search.
+							</p>
+						)}
 					</div>
 				</div>
 			</div>
